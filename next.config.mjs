f7faa4067b98f4996afb9ed7ce425b673e2089df
Elvin Ega <proega3@gmail.com>
2025-03-10 16:41:17 +0300

    optimize image

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
   // output: 'export',  // Enables static export feature
   trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: ["images.unsplash.com", "prominentau.netlify.app"],
   },
 };
