888f7de759f5c09c02340498dd458bdcb05648bd
Elvin Ega <proega3@gmail.com>
2025-03-10 16:25:23 +0300

    clear image cache

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
   // output: 'export',  // Enables static export feature
   trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: ["images.unsplash.com", "prominentau.netlify.app"],
   },
 };
